3c83a1d77f1f1a2311d5a87c75eb0557be5077ac
JamesPyke <s178316@ucs.ac.uk>
2018-01-15 13:22:12 +0000

    Added new buttons with brightness change capability

--- IAssist/IAssist/MainForm.cs
+++ IAssist/IAssist/MainForm.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -16,5 +17,75 @@ namespace IAssist
         {
             InitializeComponent();
         }
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetDC(IntPtr hWnd);
+
+        [DllImport("gdi32.dll")]
+        public static extern bool SetDeviceGammaRamp(IntPtr hDC, ref RAMP lpRamp);
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+        public struct RAMP
+        {
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 256)]
+            public UInt16[] Red;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 256)]
+            public UInt16[] Green;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 256)]
+            public UInt16[] Blue;
+        }
+
+        public static void SetGamma(int gamma)
+        {
+            if (gamma <= 256 && gamma >= 1)
+            {
+                RAMP ramp = new RAMP();
+                ramp.Red = new ushort[256];
+                ramp.Green = new ushort[256];
+                ramp.Blue = new ushort[256];
+                for (int i = 1; i < 256; i++)
+                {
+                    int iArrayValue = i * (gamma + 128);
+
+                    if (iArrayValue > 65535)
+                        iArrayValue = 65535;
+                    ramp.Red[i] = ramp.Blue[i] = ramp.Green[i] = (ushort)iArrayValue;
+                }
+                SetDeviceGammaRamp(GetDC(IntPtr.Zero), ref ramp);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SetGamma(10);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SetGamma(100);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SetGamma(200);
+        }
     }
 }
+
+
+/*namespace IAssist
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+        }
+    }
+ }
+ */
