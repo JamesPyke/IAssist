55c83646bf98925fe574065770e0f28bb1057c4f
JamesPyke <s178316@ucs.ac.uk>
2018-01-31 18:04:02 +0000

    Small Welcome page chages

--- IAssist/IAssist/FrmWelcome.Designer.cs
+++ IAssist/IAssist/FrmWelcome.Designer.cs
@@ -28,33 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmWelcome));
             this.BtnLaunch = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // BtnLaunch
             // 
-            this.BtnLaunch.Location = new System.Drawing.Point(92, 165);
+            this.BtnLaunch.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnLaunch.Location = new System.Drawing.Point(12, 72);
             this.BtnLaunch.Name = "BtnLaunch";
-            this.BtnLaunch.Size = new System.Drawing.Size(75, 23);
+            this.BtnLaunch.Size = new System.Drawing.Size(220, 69);
             this.BtnLaunch.TabIndex = 0;
-            this.BtnLaunch.Text = "button1";
+            this.BtnLaunch.Text = "Enter";
             this.BtnLaunch.UseVisualStyleBackColor = true;
             this.BtnLaunch.Click += new System.EventHandler(this.BtnLaunch_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(56, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 39);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "IAssist";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // FrmWelcome
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(486, 261);
+            this.ClientSize = new System.Drawing.Size(243, 149);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.BtnLaunch);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "FrmWelcome";
-            this.Text = "FrmWelcome";
+            this.Text = "IAssist";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button BtnLaunch;
+        private System.Windows.Forms.Label label1;
     }
 }
